7378e79ac80babc44add5b6ddc8b8bbfdb2e7c45
Creativi <creativi.64.64@gmail.com>
2023-10-12 21:52:08 +0200

    rem wizzard

--- aquarusshop6-5-3/xtWizard/scripts/update-4.2.00-5.0.08/update_categories.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `##_categories` ADD COLUMN `categories_level` TINYINT UNSIGNED NOT NULL DEFAULT '0';
-
-ALTER TABLE `##_categories` ADD INDEX `bb_nested_set_performance_categories_left` (`categories_left`);
-
-  
