8fdb156a45208c76b0b7518e6ade4bb9786701f0
Creativi <creativi.64.64@gmail.com>
2025-01-19 15:12:53 +0100

    admin fix

--- aq_timed_banner/hooks/page_registry_bottom.php
+++ aq_timed_banner/hooks/page_registry_bottom.php
@@ -2,56 +2,34 @@
 defined('_VALID_CALL') or die('Direct Access is not allowed.');
 
 if (defined('_SYSTEM_ADMIN')) {
-    // Register admin menu item
     $admin_menu[] = array(
-        'MenuText' => 'Timed Banners',
-        'MenuLink' => 'adminHandler.php?plugin=aq_timed_banner&load_section=AqTimedBanner',
-        'MenuIcon' => 'images/icons/clock.png',
-        'MenuSection' => 'marketing'
+        'text' => 'Timed Banners',
+        'link' => 'ejsadmin.php?plugin=aq_timed_banner&page=banners', 
+        'image' => 'images/icons/clock.png',
+        'node_group' => 'marketing'
     );
     
-    // Register admin page handler
-    if ($_GET['plugin'] == 'aq_timed_banner' && $_GET['load_section'] == 'AqTimedBanner') {
-        require_once _SRV_WEBROOT._SRV_WEB_PLUGINS.'aq_timed_banner/classes/class.aq_timed_banner_admin.php';
+    if ($_REQUEST['plugin'] == 'aq_timed_banner') {
+        include_once _SRV_WEBROOT._SRV_WEB_PLUGINS.'aq_timed_banner/classes/class.aq_timed_banner_admin.php';
         
-        $banner = new aq_timed_banner_admin();
+        $banners = new aq_timed_banner_admin();
+        $page = new Template();
         
-        if (isset($_GET['new'])) {
-            $data = array();
-        } else if (isset($_GET['edit_id'])) {
-            $data = $banner->_get((int)$_GET['edit_id']);
-        } else if (isset($_GET['save'])) {
-            if ($_POST['banner_id']) {
-                $banner->_set($_POST, 'edit');
+        if (isset($_REQUEST['page']) && $_REQUEST['page'] == 'banners') {
+            if (isset($_REQUEST['new']) || isset($_REQUEST['edit_id'])) {
+                $page->getTemplatePath('admin_timed_banners_edit.html', 'aq_timed_banner');
+                $obj = isset($_REQUEST['edit_id']) ? $banners->_get($_REQUEST['edit_id']) : array();
+                $page_data = array(
+                    'data' => $obj,
+                    'languages' => $language->_getLanguageList()
+                );
             } else {
-                $banner->_set($_POST, 'new');
+                $page->getTemplatePath('admin_timed_banners.html', 'aq_timed_banner');
+                $page_data = array(
+                    'banners' => $banners->_getList()
+                );
             }
-            header('Location: adminHandler.php?plugin=aq_timed_banner&load_section=AqTimedBanner');
-            exit;
-        } else if (isset($_GET['delete_id'])) {
-            $banner->_delete((int)$_GET['delete_id']);
-            header('Location: adminHandler.php?plugin=aq_timed_banner&load_section=AqTimedBanner');
-            exit;
+            $admin_content = $page->getTemplate('admin', $page->template_file, $page_data);
         }
-        
-        // Set template data
-        global $language;
-        $template = new Template();
-        
-        if (isset($_GET['new']) || isset($_GET['edit_id'])) {
-            $template->getTemplatePath('/admin_timed_banners_edit.html', 'aq_timed_banner');
-            $tpl_data = array(
-                'data' => $data,
-                'languages' => $language->_getLanguageList()
-            );
-        } else {
-            $template->getTemplatePath('/admin_timed_banners.html', 'aq_timed_banner');
-            $tpl_data = array(
-                'banners' => $banner->_getList()
-            );
-        }
-        
-        $template_content = $template->getTemplate('admin', $template->template_file, $tpl_data);
-        $page_data = array('content' => $template_content);
     }
 }
