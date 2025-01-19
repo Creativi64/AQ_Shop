d265f31975c0ec81c92fc6c8352dc66b6dfbb71e
Creativi <creativi.64.64@gmail.com>
2025-01-19 14:59:54 +0100

    fix

--- aq_timed_banner/classes/class.aq_timed_banner_admin.php
+++ aq_timed_banner/classes/class.aq_timed_banner_admin.php
@@ -114,4 +114,74 @@ class aq_timed_banner_admin {
         }
         return false;
     }
+
+    function _get($id) {
+        global $db;
+        
+        if ($id === 'new') {
+            return array();
+        }
+        
+        $sql = "SELECT * FROM ".DB_PREFIX."_aq_timed_banner WHERE banner_id = ?";
+        $result = $db->Execute($sql, array((int)$id));
+        
+        if ($result->RecordCount() > 0) {
+            $data = $result->fields;
+            
+            // Get language data
+            $sql = "SELECT * FROM ".DB_PREFIX."_aq_timed_banner_description 
+                   WHERE banner_id = ?";
+            $result = $db->Execute($sql, array((int)$id));
+            
+            while (!$result->EOF) {
+                $data['banner_title_'.$result->fields['language_code']] = $result->fields['banner_title'];
+                $data['banner_description_'.$result->fields['language_code']] = $result->fields['banner_description'];
+                $result->MoveNext();
+            }
+            
+            return $data;
+        }
+        return false;
+    }
+    
+    function _set($data, $mode = 'edit') {
+        global $db;
+        
+        // Handle file upload if new image
+        if (isset($_FILES['banner_image']) && $_FILES['banner_image']['size'] > 0) {
+            $upload_dir = _SRV_WEBROOT.'media/images/banners/';
+            $filename = time() . '_' . $_FILES['banner_image']['name'];
+            
+            if (move_uploaded_file($_FILES['banner_image']['tmp_name'], $upload_dir.$filename)) {
+                $data['banner_image'] = 'banners/'.$filename;
+            }
+        }
+        
+        if ($mode == 'new') {
+            $data['date_added'] = date('Y-m-d H:i:s');
+            $db->AutoExecute(DB_PREFIX.'_aq_timed_banner', $data, 'INSERT');
+            $banner_id = $db->Insert_ID();
+        } else {
+            $db->AutoExecute(DB_PREFIX.'_aq_timed_banner', $data, 'UPDATE', 'banner_id = '.(int)$data['banner_id']);
+            $banner_id = $data['banner_id'];
+        }
+        
+        // Save language data
+        global $language;
+        foreach ($language->_getLanguageList() as $lang) {
+            $lang_data = array(
+                'banner_id' => $banner_id,
+                'language_code' => $lang['code'],
+                'banner_title' => $data['banner_title_'.$lang['code']],
+                'banner_description' => $data['banner_description_'.$lang['code']]
+            );
+            
+            $db->AutoExecute(DB_PREFIX.'_aq_timed_banner_description', 
+                            $lang_data, 
+                            'REPLACE',
+                            'banner_id = '.(int)$banner_id.' AND language_code = "'.$lang['code'].'"');
+        }
+        
+        return true;
+    }
 }
--- aq_timed_banner/hooks/page_registry_bottom.php
+++ aq_timed_banner/hooks/page_registry_bottom.php
@@ -1,36 +1,57 @@
 <?php
 defined('_VALID_CALL') or die('Direct Access is not allowed.');
 
-if (defined('_SYSTEM_ADMIN'))
-{
+if (defined('_SYSTEM_ADMIN')) {
+    // Register admin menu item
     $admin_menu[] = array(
         'MenuText' => 'Timed Banners',
-        'MenuLink' => 'adminHandler.php?load_section=AqTimedBanner',
-        'MenuIcon' => 'images/icons/clock.png',  
+        'MenuLink' => 'adminHandler.php?plugin=aq_timed_banner&load_section=AqTimedBanner',
+        'MenuIcon' => 'images/icons/clock.png',
         'MenuSection' => 'marketing'
     );
-
-    if ($_REQUEST['load_section'] == 'AqTimedBanner') {
-        include_once _SRV_WEBROOT._SRV_WEB_PLUGINS.'aq_timed_banner/classes/class.aq_timed_banner_admin.php';
+    
+    // Register admin page handler
+    if ($_GET['plugin'] == 'aq_timed_banner' && $_GET['load_section'] == 'AqTimedBanner') {
+        require_once _SRV_WEBROOT._SRV_WEB_PLUGINS.'aq_timed_banner/classes/class.aq_timed_banner_admin.php';
         
-        $page = new aq_timed_banner_admin();
+        $banner = new aq_timed_banner_admin();
         
-        if (isset($_REQUEST['new']) || isset($_REQUEST['edit_id'])) {
-            $template = 'admin_timed_banners_edit.html';
-            $data = isset($_REQUEST['edit_id']) ? $page->_get($_REQUEST['edit_id']) : array();
+        if (isset($_GET['new'])) {
+            $data = array();
+        } else if (isset($_GET['edit_id'])) {
+            $data = $banner->_get((int)$_GET['edit_id']);
+        } else if (isset($_GET['save'])) {
+            if ($_POST['banner_id']) {
+                $banner->_set($_POST, 'edit');
+            } else {
+                $banner->_set($_POST, 'new');
+            }
+            header('Location: adminHandler.php?plugin=aq_timed_banner&load_section=AqTimedBanner');
+            exit;
+        } else if (isset($_GET['delete_id'])) {
+            $banner->_delete((int)$_GET['delete_id']);
+            header('Location: adminHandler.php?plugin=aq_timed_banner&load_section=AqTimedBanner');
+            exit;
+        }
+        
+        // Set template data
+        global $language;
+        $template = new Template();
+        
+        if (isset($_GET['new']) || isset($_GET['edit_id'])) {
+            $template->getTemplatePath('/admin_timed_banners_edit.html', 'aq_timed_banner');
             $tpl_data = array(
                 'data' => $data,
                 'languages' => $language->_getLanguageList()
             );
         } else {
-            $template = 'admin_timed_banners.html'; 
+            $template->getTemplatePath('/admin_timed_banners.html', 'aq_timed_banner');
             $tpl_data = array(
-                'banners' => $page->_getList()
+                'banners' => $banner->_getList()
             );
         }
         
-        $admin_content = new Template();
-        $admin_content->getTemplatePath($template, 'aq_timed_banner');
-        $admin_content = $admin_content->getTemplate('admin', $template, $tpl_data);
+        $template_content = $template->getTemplate('admin', $template->template_file, $tpl_data);
+        $page_data = array('content' => $template_content);
     }
 }
