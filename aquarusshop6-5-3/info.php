ef3404e04f46018d0b391f7122b1f63d6514ca38
Creativi <creativi.64.64@gmail.com>
2025-02-15 00:59:38 +0100

    asd

--- aquarusshop6-5-3/info.php
+++ aquarusshop6-5-3/info.php
@@ -1,3 +1,32 @@
 <?php
 phpinfo();
+// $servername = "mysql_db"; // Change this if your MySQL server is remote
+// $username = "shopuser";
+// $password = "shoppass";
+// $database = "aq_shop";
+
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $database);
+
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+
+// // SQL query to fetch data
+// $sql = "SELECT * FROM xt_config"; // Change "users" to your table name
+// $result = $conn->query($sql);
+
+// if ($result->num_rows > 0) {
+     
+//     while ($row = $result->fetch_assoc()) {
+//        print_r($row);
+//     }
+    
+// } else {
+//     echo "No results found.";
+// }
+
+// // Close connection
+// $conn->close();
 ?>
--- aquarusshop6-5-3/plugins/aq_timed_banner/hooks/page_registry_bottom.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('_VALID_CALL') or die('Direct Access is not allowed.');
-
-define('TABLE_BANNER', DB_PREFIX.'_aq_timed_banner');
-define('TABLE_BANNER_DESCRIPTION', DB_PREFIX.'_aq_timed_banner_description');
- 
